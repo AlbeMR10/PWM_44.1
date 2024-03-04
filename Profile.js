b3b8d29c2755311e85725002102be96b212aeafc
CarlosMireles <carlosmirelesrodriguez1@gmail.com>
2024-03-04 18:18:53 +0000

    Perfil y headerLoggedIn

--- Profile.js
+++ Profile.js
@@ -1,5 +1,4 @@
-// Imágenes de ejemplo
-const images = [
+const leagueImages = [
     'Images/Leagues/nba.png',
     'Images/Leagues/laliga.png',
     'Images/Leagues/premier.png',
@@ -9,46 +8,23 @@ const fotoPerfil = [
     'Images/Components/profile.png'
 ];
 
-// Obtener el contenedor del carrusel
-const carouselContainer = document.querySelector('.carousel-container');
-const profilePicture = document.querySelector('.foto-de-perfil');
-// Crear el HTML para el carrusel
+const carouselContainer = document.querySelector('.leagueCarousel');
+const profilePicture = document.querySelector('.profilePicture');
+
+
 const carouselHTML = `
-    <div class="slick-carousel">
-        ${images.map(image => `<div><img src="${image}" alt=""></div>`).join('')}
+    <div class="leagueCarousel">
+        ${leagueImages.map(image => `<img src="${image}" alt="">`).join('')}
     </div>
 `;
 
 const fotoPerfillHTML = `
-    <div class="slick-carousel">
+    <div class="profilePicture">
         ${fotoPerfil.map(image => `<div><img src="${image}" alt=""></div>`).join('')}
     </div>
 `;
 
-// Insertar el HTML del carrusel en el contenedor
+
 carouselContainer.innerHTML = carouselHTML;
 profilePicture.innerHTML = fotoPerfillHTML;
 
-$('.slick-carousel').slick({
-    infinite: true,
-    slidesToShow: 3, // Mostrar 3 imágenes a la vez
-    slidesToScroll: 3, // Desplazar 3 imágenes a la vez
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2, // Mostrar 2 imágenes a la vez en dispositivos más pequeños
-                slidesToScroll: 2 // Desplazar 2 imágenes a la vez en dispositivos más pequeños
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1, // Mostrar 1 imagen a la vez en dispositivos aún más pequeños
-                slidesToScroll: 1 // Desplazar 1 imagen a la vez en dispositivos aún más pequeños
-            }
-        }
-    ]
-});
---
+++ headerLoggedIn.html.js
@@ -0,0 +1,7 @@
+const logoIcon = document.getElementById('logo-icon');
+
+// Función para minimizar el header al hacer clic en el logo
+logoIcon.addEventListener('click', function() {
+    // Aquí puedes agregar la lógica para minimizar el header
+    console.log('Header minimizado');
+});
